299e112aa8cc3a38b8110cdfd9b603785e80315c
tnielsen2 <trentnielsen84@gmail.com>
2025-08-17 12:04:02 -0400

    Init, add version to proxmox

--- on-premises-servers.tf
+++ on-premises-servers.tf
@@ -1,19 +1,8 @@
 # On-premises infrastructure - Proxmox VMs
-
 module "windows_vm1" {
   source = "./modules/proxmox-vm"
   
   vm_name       = "windows-vm-01"
-  target_node   = "proxmox"
-  template_name = "server-2019-vm-template"
-  vm_disk_size  = "100G"
-  os_type       = "windows"
-}
-
-module "windows_vm2" {
-  source = "./modules/proxmox-vm"
-  
-  vm_name       = "windows-vm-02"
   target_node   = "proxmox"
   template_name = "server-2019-vm-template"
   vm_disk_size  = "100G"
@@ -25,7 +14,7 @@ module "linux_vm1" {
   
   vm_name       = "ubuntu-vm-01"
   target_node   = "proxmox"
-  template_name = "ubuntu-2204-template"
-  vm_disk_size  = "100G"
+  template_name = "ubuntu-2404-vm-template"
+  vm_disk_size  = "150G"
   os_type       = "linux"
 }
