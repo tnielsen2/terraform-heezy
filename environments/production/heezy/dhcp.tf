a59f2e72766f0780b8bde7fe7355c89ca14a9765
tnielsen2 <trentnielsen84@gmail.com>
2025-09-27 07:43:47 -0400

    Add Talos workers K8 hosts

--- environments/production/heezy/dhcp.tf
+++ environments/production/heezy/dhcp.tf
@@ -23,6 +23,37 @@ resource "fortios_systemdhcp_server" "prod_vlan_2000" {
     mac    = "6c:4b:90:53:4b:61"
     action = "assign"
   }
+  # Talos 2
+  reserved_address {
+    id     = 1
+    ip     = "192.168.1.16"
+    mac    = "6c:4b:90:2b:9a:9c"
+    action = "assign"
+  }
+  # Talos 3
+  reserved_address {
+    id     = 1
+    ip     = "192.168.1.17"
+    mac    = "6c:4b:90:18:e5:d1"
+    action = "assign"
+  }
+  # Talos 4
+  reserved_address {
+    id     = 1
+    ip     = "192.168.1.18"
+    mac    = "6c:4b:90:54:67:5e"
+    action = "assign"
+  }
+
+  # Talos 5
+  reserved_address {
+    id     = 1
+    ip     = "192.168.1.19"
+    mac    = "6c:4b:90:55:a5:e4"
+    action = "assign"
+  }
+
+
 
   depends_on = [fortios_system_interface.prod_vlan_2000]
 }
