c656cc9d8a540051f1c4d5e790884e5003ed91ed
tnielsen2 <trentnielsen84@gmail.com>
2025-11-09 08:13:28 -0500

    Add about website ecr repo

--- environments/production/aws/aws-ecr.tf
+++ environments/production/aws/aws-ecr.tf
@@ -72,3 +72,36 @@ resource "aws_ecr_lifecycle_policy" "github_runner" {
     ]
   })
 }
+
+# In-house developed applications
+resource "aws_ecr_repository" "about_website" {
+  name                 = "about-website"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = local.common_tags
+}
+
+resource "aws_ecr_lifecycle_policy" "about_website" {
+  repository = aws_ecr_repository.about_website.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
